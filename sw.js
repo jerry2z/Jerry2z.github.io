1d7067c78a2c5f8bce1a2c7a67a6cf024d90f305
jerry2z <1930489373@qq.com>
2025-11-27 20:43:39 +0800

    Site updated: 2025-11-27 20:43:37

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1764070751598";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1764247408066";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
